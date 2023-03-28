1e8f0f201029077b180e0c72eb5a8eaa6cd7429d
ZeynepAkagunduz <zeynepakagunduz06@gmail.com>
2023-03-28 05:44:21 +0300

    ilkGitHub

--- src/gitHup_ilkDers.java
+++ src/gitHup_ilkDers.java
@@ -1,6 +1,6 @@
 public class gitHup_ilkDers {
     public static void main(String[] args) {
-        System.out.println(" Hello GitHub ");
+        System.out.println("Hello GitHub");
 
         //git add . ---> burdaki her seyi al demek
     }
