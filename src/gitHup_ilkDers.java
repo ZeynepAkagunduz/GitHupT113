8033e208bd8233f9d6abb82e940d0c8ce1561208
ZeynepAkagunduz <zeynepakagunduz06@gmail.com>
2023-03-30 06:43:48 +0300

    ikincigun

--- src/gitHup_ilkDers.java
+++ src/gitHup_ilkDers.java
@@ -4,6 +4,36 @@ public class gitHup_ilkDers {
         System.out.println(" -------> ");
         System.out.println("have a wise day");
 
+        /*
+        cd.. --> oldugun klasörden çıkabilirsin
+
+        C klasör'ünün içinde ne olduguna bakmak istersen ---> dir
+
+        bakmak istedigin klasör için --> cd klasör adi
+
+        yeni proje oluşturmak için ---> mkdir + klasör ismi
+
+        githup113 bos klasöründe git init yazdik direk git. ile oluşturdu
+        artık bu klasör git ile direk bağlantılı.
+
+        git branch -M main --> ana branch'de çalıştığımı söylüyorum
+
+
+         C:\Users\ASUS\IdeaProjects\GitHub02>git remote add origin + link baglantı --> olusturdugun repo'yu tanımlamak
+          bunu zaten tanımladın hatası veriyorsa ADD YERİNE == set-url yazılır
+
+          git commit
+          git push
+
+          githup'da yeni dosya yuklediginde dosya yanındaki ... 'ya basınca refarans numarası çıkar
+
+          git status --> durumun ne oldugunu söyler
+
+
+
+
+         */
+
 
     }
 }
